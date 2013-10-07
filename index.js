02dcd29cddaeaf54a67f6fa77200b4c43b18666b
s0205128 <jan.weyens@student.khleuven.be>
2013-10-07 12:59:21 +0200

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function display() {
+	var naam = document.getElementById("text").value;
+	var resultaat = document.getElementById("result");
+	resultaat.innerHTML = "Hello " + naam + "! Have a nice day";
+}
